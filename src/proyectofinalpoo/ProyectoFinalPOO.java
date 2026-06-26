cb3f614c82da0ce64c9c099332fcb40d0b9d789c
angelotacoj <angelotacoj@gmail.com>
2026-06-26 09:26:05 -0500

    este es mi segundo commit

--- src/proyectofinalpoo/ProyectoFinalPOO.java
+++ src/proyectofinalpoo/ProyectoFinalPOO.java
@@ -14,7 +14,7 @@ public class ProyectoFinalPOO {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola! Este es mi segundo commit! :)");
     }
     
 }
